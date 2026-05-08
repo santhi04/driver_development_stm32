945246b2e859e7cf47101949edf2d4a3a545653b
nikhila <nikhila.pantham@microtrol.co.uk>
2026-05-08 13:47:03 +0100

    Created the header and source files for GPIO driver

---
+++ stm32_peripheral_drivers/drivers/Inc/gpio_driver.h
@@ -0,0 +1,13 @@
+/*
+ * gpio_driver.h
+ *
+ *  Created on: 8 May 2026
+ *      Author: NikhilaPantham
+ */
+
+#ifndef INC_GPIO_DRIVER_H_
+#define INC_GPIO_DRIVER_H_
+
+
+
+#endif /* INC_GPIO_DRIVER_H_ */
---
+++ stm32_peripheral_drivers/drivers/Src/gpio_driver.c
@@ -0,0 +1,9 @@
+/*
+ * gpio_driver.c
+ *
+ *  Created on: 8 May 2026
+ *      Author: NikhilaPantham
+ */
+
+
+#include "gpio_driver.h"
