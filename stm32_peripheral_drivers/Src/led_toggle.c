8a41f2ee94d35c624d1c8e414aeef7cc160d9930
santhi04 <52497586+santhi04@users.noreply.github.com>
2026-05-10 22:18:43 +0100

    Deleted the actual main file and created another which blinks a specified LED

---
+++ stm32_peripheral_drivers/Src/led_toggle.c
@@ -0,0 +1,38 @@
+/*
+ * led_toggle.c
+ *
+ *  Created on: 10 May 2026
+ *      Author: nikhi
+ */
+
+#include <stdint.h>
+#include "stm32f401xx.h"
+#include "gpio_driver.h"
+
+void delay()
+{
+   for(int i = 0; i < 250000; i++);
+}
+
+
+int main(void)
+{
+    gpiox_handle_t gpio_led;
+    gpio_led.gpiox_base_addr = GPIOA;
+    gpio_led.gpio_pin_config.gpiox_pin_number = GPIO_PIN_NO_5;
+    gpio_led.gpio_pin_config.gpiox_pin_mode = GPIO_MODE_OUTPUT;
+    gpio_led.gpio_pin_config.gpiox_pin_optype = GPIO_OP_TYPE_PP;
+    gpio_led.gpio_pin_config.gpiox_pin_pupd_ctrl = GPIO_NO_PU_PD;
+    gpio_led.gpio_pin_config.gpiox_pin_speed = GPIO_SPEED_FAST;
+
+    gpio_pclk_ctrl(GPIOA, ENABLE);
+    gpio_init(&gpio_led);
+
+    while(1)
+    {
+       gpio_toggle_pin(GPIOA, GPIO_PIN_NO_5);
+       delay();
+    }
+    
+    return 0;
+}
